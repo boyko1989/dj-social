a9f05946087ab459f2dc1e947b372d4fa64ab574
boyko1989 <hunwiss89@gmail.com>
2023-01-21 21:20:03 +0400

    Added django-allauth

--- social/settings.py
+++ social/settings.py
@@ -8,11 +8,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
+
+# python-dotenv
+# https://pypi.org/project/python-dotenv/
 from dotenv import load_dotenv
 
 # Loading ENV
 env_path = Path('.')/'.env'
 load_dotenv(dotenv_path=env_path)
+# END python-dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,13 +43,29 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.vk',
+    'allauth.socialaccount.providers.yandex',
+    'allauth.socialaccount.providers.telegram',
+    'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.auth0',
+
     'blog.apps.BlogConfig',
 
     # https://django-crispy-forms.readthedocs.io/en/latest/install.html
     'crispy_forms',
+    # https://pypi.org/project/django-ckeditor/#installation
+    'ckeditor',
     # https://pypi.org/project/django-cleanup/
     'django_cleanup.apps.CleanupConfig',
+    # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
+    'django.contrib.sites',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -61,8 +82,7 @@ ROOT_URLCONF = 'social.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,6 +94,52 @@ TEMPLATES = [
         },
     },
 ]
+
+# django-allauth
+# https://django-allauth.readthedocs.io/en/latest/installation.html
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+# SOCIALACCOUNT_PROVIDERS = {
+#     'google': {
+#         # For each OAuth based provider, either add a ``SocialApp``
+#         # (``socialaccount`` app) containing the required client
+#         # credentials, or list them here:
+#         'APP': {
+#             'client_id': '123',
+#             'secret': '456',
+#             'key': ''
+#         }
+#     }
+# }
+
+SOCIALACCOUNT_PROVIDERS = {
+
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        },
+    },
+    'github': {
+        'SCOPE': [
+            'user',
+            'repo',
+            'read.org',
+        ],
+    },
+
+}
+
+# END django-allauth
 
 WSGI_APPLICATION = 'social.wsgi.application'
 
@@ -143,3 +209,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'uni_form'
 # END django-crispy-forms
+
+# django-ckeditor
+# https://pypi.org/project/django-ckeditor/#installation
+CKEDITOR_CONFIG = {
+    'default': {
+        'width': 'auto',
+    }
+}
+
+# END django-ckeditor
--- social/urls.py
+++ social/urls.py
@@ -14,11 +14,22 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # path('register/', user_wievs.register, name='register'),
+    # path('profile/', user_views.profile, name='profile'),
+    # path('login/', auth_views.LoginView.as_view(temmplate_name='users/login.html'), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    # path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    # path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    # path('password-reset-confirm/<uid64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    # path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+
+    path('accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
